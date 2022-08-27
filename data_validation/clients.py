8e5582223a18c9191a5bf81807c3fb5e2a9c0039
Brandon Geise <branondgeise@gmail.com>
2022-08-27 08:58:06 -0400

    Fix client lookup

--- data_validation/clients.py
+++ data_validation/clients.py
@@ -245,16 +245,16 @@ def get_data_client(connection_config):
 
 
 CLIENT_LOOKUP = {
-    "BigQuery": ibis_bigquery.name,
-    "Impala": impala_connect.name,
-    "MySQL": MySQLClient.name,
-    "Oracle": OracleClient.name,
-    "FileSystem": get_pandas_client.name,
-    "Postgres": PostgreSQLClient.name,
-    "Redshift": PostgreSQLClient.name,
-    "Teradata": TeradataClient.name,
-    "MSSQL": MSSQLClient.name,
-    "Snowflake": snowflake_connect.name,
-    "Spanner": spanner_connect.name,
-    "DB2": DB2Client.name,
+    "BigQuery": ibis_bigquery.Backend.name,
+    # "Impala": impala_connect.Backend.name,
+    "MySQL": MySQLClient.Backend.name,
+    # "Oracle": OracleClient.Backend.name,
+    # "FileSystem": get_pandas_client.name,
+    "Postgres": PostgreSQLClient.Backend.name,
+    "Redshift": PostgreSQLClient.Backend.name,
+    # "Teradata": TeradataClient.name,
+    "MSSQL": MSSQLClient.Backend.name,
+    # "Snowflake": snowflake_connect.name,
+    # "Spanner": spanner_connect.name,
+    # "DB2": DB2Client.name,
 }
